862dd4c8c0fcf325aa0ed2c35d5028b9d6c333e2
Eugene Nguyen <eugenenguyen314@gmail.com>
2021-04-16 22:34:36 +0700

    New login page

---
+++ main/login.php
@@ -0,0 +1,41 @@
+<title>Đăng nhập</title>
+<div class="d-login-bg"></div>
+<div class="d-login-container">
+    <div class="login-box">
+        <div class="login-title">Đăng nhập</div>
+        <form class="login-form" id="loginForm">
+            <div class="collab-input">
+                <div class="input-container">
+                    <div class="input-error-content"></div>
+                    <div class="input-icon">
+                        <ion-icon name="person"></ion-icon>
+                    </div>
+                    <div class="input-main">
+                        <input type="text" name="userName" placeholder="Nhập tên đăng nhập của bạn">
+                        <label>Tên đăng nhập</label>
+                    </div>
+                </div>
+                <div class="input-container">
+                    <div class="input-error-content"></div>
+                    <div class="input-icon">
+                        <ion-icon name="key"></ion-icon>
+                    </div>
+                    <div class="input-main">
+                        <input type="text" name="password" placeholder="Nhập mật khẩu của bạn">
+                        <label>Mật khẩu</label>
+                    </div>
+                </div>
+            </div>
+            <div class="collab-checkbox-container">
+                <label class="collab-checkbox">
+                    <input type="checkbox" name="remember">
+                    <span class="checkmark"></span>
+                    <div class="checkbox-title">Ghi nhớ thiết bị</div>
+                </label>
+            </div>
+            <button class="collab-button">Đăng nhập</button>
+
+            <div class="login-signup"><a href="/signup">Đăng ký tài khoản</a></div>
+        </form>
+    </div>
+</div>
