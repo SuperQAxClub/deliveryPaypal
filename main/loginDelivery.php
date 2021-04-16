be9adc9c0350cbede0623faa2d9ab57783d7afc6
Eugene Nguyen <eugenenguyen314@gmail.com>
2021-04-16 22:41:49 +0700

    New login page for employee

---
+++ main/loginDelivery.php
@@ -0,0 +1,39 @@
+<title>Đăng nhập nhân viên</title>
+<div class="d-login-bg"></div>
+<div class="d-login-container">
+    <div class="login-box">
+        <div class="login-title">Đăng nhập nhân viên</div>
+        <form class="login-form" id="loginDeliveryForm">
+            <div class="collab-input">
+                <div class="input-container">
+                    <div class="input-error-content"></div>
+                    <div class="input-icon">
+                        <ion-icon name="person"></ion-icon>
+                    </div>
+                    <div class="input-main">
+                        <input type="text" name="userName" placeholder="Nhập tên đăng nhập của bạn">
+                        <label>Tên đăng nhập</label>
+                    </div>
+                </div>
+                <div class="input-container">
+                    <div class="input-error-content"></div>
+                    <div class="input-icon">
+                        <ion-icon name="key"></ion-icon>
+                    </div>
+                    <div class="input-main">
+                        <input type="password" name="password" placeholder="Nhập mật khẩu của bạn">
+                        <label>Mật khẩu</label>
+                    </div>
+                </div>
+            </div>
+            <div class="collab-checkbox-container">
+                <label class="collab-checkbox">
+                    <input type="checkbox" name="remember">
+                    <span class="checkmark"></span>
+                    <div class="checkbox-title">Ghi nhớ thiết bị</div>
+                </label>
+            </div>
+            <button type="submit" class="collab-button full-width">Đăng nhập</button>
+        </form>
+    </div>
+</div>
